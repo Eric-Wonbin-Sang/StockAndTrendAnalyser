36c8b70de1e328965d38961bd93ef4e46f147d95
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-12-19 20:24:23 -0500

    In trend_analysis.py, added a graph_individually parameter for show_trends function.

--- trend_analysis.py
+++ trend_analysis.py
@@ -10,21 +10,9 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 
 
-def group_list_by_size(trend_list, list_size):
-    # This groups any one dimensional list and groups it into lists with length of up to list_size
-    if not trend_list:
-        return []
-    if len(trend_list) < list_size:
-        return [trend_list]
-    data_list_list = list([list(x) for x in zip(*[iter(trend_list)] * list_size)])
-    if len(trend_list) % list_size != 0:
-        data_list_list.append(trend_list[-(len(trend_list) % list_size):])
-    return data_list_list
-
-
 def get_trend_df_list(init_trend_list):
 
-    trend_list_list = group_list_by_size(init_trend_list, 5)
+    trend_list_list = Functions.group_list_by_size(init_trend_list, 5)
     py_trends = TrendReq(hl='en-US', tz=360, timeout=(10, 25))
     trends_df_list = []
     for trend_list in trend_list_list:
@@ -32,7 +20,7 @@ def get_trend_df_list(init_trend_list):
         py_trends.build_payload(
             trend_list,
             cat=0,
-            timeframe="2019-01-01 2020-12-15",
+            timeframe="2017-01-01 2020-12-15",
             geo="US",
             gprop=""
         )
@@ -46,14 +34,20 @@ def get_trend_df_list(init_trend_list):
     return trends_df_list
 
 
-def show_trends(init_trend_list):
+def show_trends(init_trend_list, graph_individually=False):
+
+    def update_and_show_graph():
+        plt.xlabel("Date")
+        plt.ylabel("Searches")
+        plt.title("Google Trends")
+        plt.legend(loc="upper left")
+        plt.grid()
+
+        plt.show()
 
     trend_df_list = get_trend_df_list(init_trend_list)
 
     for trend, trend_df in zip(init_trend_list, trend_df_list):
-
-        rolling_name = "{} Rolling".format(trend)
-        trend_df[rolling_name] = trend_df[trend].rolling(window=30).mean()
 
         x_list = trend_df.index.factorize()[0].reshape(-1, 1)
         y_list = trend_df[trend].values
@@ -62,19 +56,16 @@ def show_trends(init_trend_list):
         y_predicted_list = linear_regression.predict(x_list)
 
         Functions.graph_x_list_and_y_list(trend_df.index, trend_df[trend], trend)
-        # graph_x_list_and_y_list(trend_df.index, trend_df[rolling_name], rolling_name)
         Functions.graph_x_list_and_y_list(trend_df.index, y_predicted_list, "{} linear regression: {}".format(
             trend,
             "+" if (y_predicted_list[-1] - y_predicted_list[0]) / 2 > 0 else "-"
         ))
 
-        plt.xlabel("Date")
-        plt.ylabel("Searches")
-        plt.title("Trends")
-        plt.legend(loc="upper left")
-        plt.grid()
+        if graph_individually:
+            update_and_show_graph()
 
-        plt.show()
+    if not graph_individually:
+        update_and_show_graph()
 
 
-show_trends(["prime", "shipping", "work from home", "online shopping", "delivery", "restaurants"])
+show_trends(["prime", "shipping", "online shopping", "delivery", "amazon"][-1:], graph_individually=True)
