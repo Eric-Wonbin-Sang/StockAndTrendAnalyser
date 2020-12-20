2b25bd0098145c8b51cc957bc39f2854f05f6db8
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-12-19 20:23:56 -0500

    Moved trend_analysis.py's group_list_by_size to Functions.py.

--- General/Functions.py
+++ General/Functions.py
@@ -3,3 +3,15 @@ import matplotlib.pyplot as plt
 
 def graph_x_list_and_y_list(x_list, y_list, label):
     plt.plot(x_list, y_list, label=label)
+
+
+def group_list_by_size(trend_list, list_size):
+    # This groups any one dimensional list and groups it into lists with length of up to list_size
+    if not trend_list:
+        return []
+    if len(trend_list) < list_size:
+        return [trend_list]
+    data_list_list = list([list(x) for x in zip(*[iter(trend_list)] * list_size)])
+    if len(trend_list) % list_size != 0:
+        data_list_list.append(trend_list[-(len(trend_list) % list_size):])
+    return data_list_list
